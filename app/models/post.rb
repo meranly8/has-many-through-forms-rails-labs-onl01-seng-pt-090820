1e1925e95111db372a7dfe8e85ffe95baf374d1e
Michelle Ranly <meranly8@gmail.com>
2021-01-27 23:44:41 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,17 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  
+  accepts_nested_attributes_for :comments
 
-
+  def categories_attributes=(categories)
+    categories.each do |i, attrs|
+      if attrs[:name].present?
+        category = Category.find_or_create_by(name: attrs[:name])
+          if !self.categories.include?(category)
+            self.post_categories.build(category: category)
+          end
+      end
+    end
+  end
 end
